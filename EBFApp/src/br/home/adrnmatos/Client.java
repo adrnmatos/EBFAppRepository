e51036db1ef552316ee44d8ffb5c7b69152abaeb
Adriano Matos <adrnmatos@gmail.com>
2020-03-02 18:49:11 -0400

    project to be deleted.

--- EBFApp/src/br/home/adrnmatos/Client.java
+++ EBFApp/src/br/home/adrnmatos/Client.java
@@ -1,32 +1,124 @@
 package br.home.adrnmatos;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
 public class Client {
 	
-	private List<Plan> planList = new ArrayList<Plan>();
+	@Id
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
+	private int id;
 	
+	private String nome;
+	private String cpf;
+	private String rg;
+	private String email;
+	private String telefone;
+	
+	private Set<Endereco> enderecos = new HashSet<>();
+
+	private Set<Plano> planList = new HashSet<>();
+
+
+	public Client() {
+		
+	}
+	
+	public Client(String nome) {
+		this.nome = nome;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public Set<Endereco> getEnderecos() {
+		return enderecos;
+	}
+
+	public void setEnderecos(Set<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}
+
+	public Set<Plano> getPlanList() {
+		return planList;
+	}
+
+	public void setPlanList(Set<Plano> planList) {
+		this.planList = planList;
+	}
+
 	public boolean isActive() {
-		if(planList.isEmpty())
-			return false;
-		else {
-			if(getActivePlan() != null)
-				return true;
-			return false;
-		}
+
+		if(planList.isEmpty()) return false;
+
+		if(getActivePlan() == null) return false;
+
+		return true;
+		
 	}
 
-	public void assignPlan(Plan p) {
-		this.planList.add(p);		
+	public void assignPlan(Plano p) {
+	
+		this.planList.add(p);
 	}
 
-	public Plan getActivePlan() {
-		for(Plan p : planList) {
-			if(p.isActive())
-				return p;
-		}
+	public Plano getActivePlan() {
+	
 		return null;
+		
 	}
-
+	  
 }
---
+++ EBFApp/src/br/home/adrnmatos/Endereco.java
@@ -0,0 +1,103 @@
+package br.home.adrnmatos;
+
+public class Endereco {
+
+	
+	private int id;
+	private Client client;
+	private String pais ;
+	private String estado;
+	private String cidade;
+	private String logradouro;
+	private String numero;
+	private String complemento;
+	private String bairro;
+	private String CEP;
+	
+	
+	public Endereco() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public Client getClient() {
+		return client;
+	}
+	
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	
+	public String getPais() {
+		return pais;
+	}
+	
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	
+	public String getEstado() {
+		return estado;
+	}
+	
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	public String getCidade() {
+		return cidade;
+	}
+	
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	
+	public String getLogradouro() {
+		return logradouro;
+	}
+	
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	
+	public String getNumero() {
+		return numero;
+	}
+	
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	public String getComplemento() {
+		return complemento;
+	}
+	
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	
+	public String getBairro() {
+		return bairro;
+	}
+	
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	
+	public String getCEP() {
+		return CEP;
+	}
+	
+	public void setCEP(String cEP) {
+		CEP = cEP;
+	}
+	
+	
+}
